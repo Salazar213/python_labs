83f192d771bb7d912ae6453cff1b883a7dc2ed56
Salazar213 <Merk_D@inbox.ru>
2025-09-19 11:20:51 +0300

    lab01_fix

--- src/lab01/ex04.py
+++ src/lab01/ex04.py
@@ -1,2 +1,9 @@
 m = int(input("Минуты "))
-print(f"{m//60}:{(m%60):02d}")
+days = 0
+if m <1440:
+    print(f"{m//60}:{(m%60):02d}")
+else:
+    while m>1440:
+        m-=1440
+        days+=1
+print(f"{days}\n{m//60}:{(m%60):02d}")
