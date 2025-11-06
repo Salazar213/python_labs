7603c1580ce66aa94a3fdf095682c97871bc5fbd
Salazar213 <Merk_D@inbox.ru>
2025-11-07 01:01:23 +0300

    lab06_convert,text

--- src/lab06/cli_text.py
+++ src/lab06/cli_text.py
@@ -1,5 +1,6 @@
-from lib.text import read_text
+from lib.text import count_freq, count_freq_top,read_text,normalize,tokenize,top_n
 import argparse
+from pathlib import Path
 
 def main():
     parser = argparse.ArgumentParser(description="CLI‑утилиты лабораторной №6")
@@ -18,8 +19,42 @@ def main():
     args = parser.parse_args()
 
     if args.command == "cat":
-        args_cat = cat_parser.parse_args()
-        cat_input = args_cat.input
-        """ Реализация команды cat """
+        cat_input = args.input
+        cat_n = args.n
+        i_stroka = 1
+        cat_input = Path(cat_input)
+        if not(cat_input.exists()):
+            raise FileNotFoundError('Файл не найден')
+        try:
+            with cat_input.open('r',encoding = 'utf-8') as d:
+                if cat_n:
+                    for line in d.readlines():
+                        print(f'{i_stroka} строка: {line}',end='')
+                        i_stroka+=1
+                else:
+                    for line in d.readlines():
+                        print(f'{line}',end='')
+        except:
+            raise UnicodeDecodeError('Не удалось прочитать файл')
+            """ Реализация команды cat """
     elif args.command == "stats":
+        stars_input = args.input
+        stats_top_n = args.top
+        stars_input = Path(stars_input)
+        if not(stars_input.exists()):
+            raise FileNotFoundError(f"Файл не найден по пути {stars_input}")
+        try:
+            text = read_text(path= stars_input)
+        except:
+            raise UnicodeEncodeError("Ошибка чтения файла")
+        if not(type(stats_top_n) == int):
+            raise TypeError(f"Ошибка type(n) = {type(stats_top_n)}, а должен быть int")
+
+        text_normalize = normalize(text)
+        text_tokenize = tokenize(text_normalize)
+        text_freq = count_freq(text_tokenize)
+        text_top = top_n(text_freq,stats_top_n)
+        print('word','count')
+        for word, count in text_top:
+            print(word,count)
         """ Реализация команды stats """
--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -1,3 +1,3 @@
-from .text import normalize, tokenize, count_freq, top_n, read_text, write_csv
+from .text import normalize, tokenize, count_freq, top_n, read_text, write_csv,count_freq_top,csv_to_xlsx,csv_to_json,json_to_csv
 
-__all__ = ['normalize', 'tokenize', 'count_freq', 'top_n','read_text', 'write_csv']
+__all__ = ['normalize', 'tokenize', 'count_freq', 'top_n','read_text', 'write_csv','count_freq_top','csv_to_xlsx','csv_to_json','json_to_csv']
